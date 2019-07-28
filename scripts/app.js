1b1defa63b2cb0c63e1ca10fcaffbd5b0d1e1527
o1jack1o <komoep21@gmail.com>
2019-07-29 00:23:51 +0300

    foo

--- scripts/app.js
+++ scripts/app.js
@@ -1,89 +1,95 @@
-// const sum = [...document.querySelectorAll('tr > td:last-child')]
-//     .map(el => Number(el.innerHTML)).reduce((acc, el) => acc+el,0);
-
-
-// const html = document.createElement('div');
-// html.innerText = sum + '$';
-
-// // document.getElementsByClassName('col-12')[0].appendChild(html);
-
-// document.querySelector('table > tbody > tr:last-child > td:last-child')
-// .innerText = sum + '$';
-
-
-const users = [
-    {name: 'Mark', gender: 'male', salary: '1500'},
-    {name: 'Igor', gender: 'male', salary: '500'},
-    {name: 'Anna', gender: 'female', salary: '1000'},
-    {name: 'John', gender: 'male', salary: '2100'},
-    {name: 'Oleh', gender: 'male', salary: '3000'},
-    {name: 'Mary', gender: 'female', salary: '1500'},
-    {name: 'Pit', gender: 'male', salary: '750'},
-    {name: 'Bred', gender: 'male', salary: '1050'},
-    {name: 'Tom', gender: 'male', salary: '1500'},
-    {name: 'Jery', gender: 'female', salary: '1800'},
-    {name: 'Serj', gender: 'male', salary: '2000'},
-];
-
-const innerTablesRows = users.map((el, i) => {
-    const tr = document.createElement('tr');
+let users = [];
+const createElement = (tag, content, className=null, attr=[]) => {
+    const element = document.createElement(tag);
     
-    const index = document.createElement('th');
-    index.innerText = i + 1;
-    tr.appendChild(index);
+    if(typeof content == 'object'){
+        element.appendChild(content)
+    }else{
+        element.innerText = content;
+    }
 
-    const name = document.createElement('td');
-    name.innerText = el.name;
-    tr.appendChild(name);
+    if(attr.length){
+        attr.map(el => element.setAttribute(el.name,el.value))
+    }
+    
+    element.className = className?className:'';
 
-    const gender = document.createElement('td');
-    gender.innerText = el.gender;
-    tr.appendChild(gender);
+    return element;
+}
+{/* <i class="fas fa-spinner fa-spin"></i> */}
 
-    const salary = document.createElement('td');
-    salary.innerText = el.salary;
-    tr.appendChild(salary);
+const getUsersData = () => {
+    document.getElementsByClassName('col-12')[0].innerHTML = '';
+    document.getElementsByClassName('col-12')[0].appendChild(
+        createElement('i','','fas fa-spinner fa-spin')
+    )
+    .appendChild(
+        createElement('span','Loading...')
+    )
+    setTimeout(()=>{
+        users = [
+            {name: 'Mark', gender: 'male', salary: '1500'},
+            {name: 'Igor', gender: 'male', salary: '500'},
+            {name: 'Anna', gender: 'female', salary: '1000'},
+            {name: 'John', gender: 'male', salary: '2100'},
+            {name: 'Oleh', gender: 'male', salary: '3000'},
+            {name: 'Mary', gender: 'female', salary: '1500'},
+            {name: 'Pit', gender: 'male', salary: '750'},
+            {name: 'Bred', gender: 'male', salary: '1050'},
+            {name: 'Tom', gender: 'male', salary: '1500'},
+            {name: 'Jery', gender: 'female', salary: '1800'},
+            {name: 'Serj', gender: 'male', salary: '2000'},
+        ];
+        console.log('uploaded')
+        render(users);
+    },2000)
+}
 
-    return tr;
-});
+const render = data => {
+    
+    
+    const appendArray = (htmlEl, arrayEls) => {
+        arrayEls.map(el => htmlEl.appendChild(el))
+        return htmlEl;
+    }
+    
+    const innerTablesRows = data.map((el, i) => {
+        const button = createElement('button','Delete','btn btn-danger',[{name:'id',value:i}]);
+        
+        button.addEventListener('click', (e) => {
+            render(data.filter((el,j) => i != j));
+        })
+        return appendArray(
+            document.createElement('tr'),
+            [
+                {tag:'th',value:i + 1},
+                {tag:'td',value:el.name},
+                {tag:'td',value:el.gender},
+                {tag:'td',value:el.salary},
+                {tag:'td',value:button},
+            ].map(el => createElement(el.tag,el.value))
+        );
+    });
+    
+    
+    const tbody = document.createElement('tbody')
+    innerTablesRows.map(el => tbody.appendChild(el));
+    
+    
+    const tr = appendArray(document.createElement('tr'),[
+            '#','Name','Gender','Salary','Action',
+        ].map(el => createElement('th',el))
+    );
+    
+    const thead = appendArray(document.createElement('thead'),[tr]);
+    
+    const table = appendArray(createElement('table','','table'),[
+        thead,tbody
+    ]);
+    document.getElementsByClassName('col-12')[0].innerHTML = '';
+    document.getElementsByClassName('col-12')[0].appendChild(table);
+}
 
 
-const tbody = document.createElement('tbody')
-innerTablesRows.map(el => tbody.appendChild(el));
 
-
-// <thead>
-//    <tr>
-//      <th scope="col">#</th>
-//      <th scope="col">Name</th>
-//      <th scope="col">Gender</th>
-//      <th scope="col">Salary</th>
-//    </tr>
-// </thead>
-
-const tr = document.createElement('tr');
-const index = document.createElement('th');
-index.innerText = '#';
-tr.appendChild(index);
-
-const name = document.createElement('th');
-name.innerText = 'Name';
-tr.appendChild(name);
-
-const gender = document.createElement('th');
-gender.innerText = 'Gender';
-tr.appendChild(gender);
-
-const salary = document.createElement('th');
-salary.innerText = 'Salary';
-tr.appendChild(salary);
-
-
-const thead = document.createElement('thead');
-thead.appendChild(tr);
-
-const table = document.createElement('table');
-table.appendChild(thead);
-table.appendChild(tbody);
-
-document.getElementsByClassName('col-12')[0].appendChild(table);
+getUsersData();
